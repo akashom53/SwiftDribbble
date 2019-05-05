c185783b8e6c960b18361f7ea20e89c406b6e286
Akash Srivastava <akashom53@gmail.com>
2019-05-05 19:21:19 +0530

    Initial Commit

---
+++ SwiftExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftExample
+//
+//  Created by Akash Srivastava on 05/05/19.
+//  Copyright © 2019 Akash Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
